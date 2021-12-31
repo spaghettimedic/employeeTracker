744861c09b320710ace6cc3ba3fa8fdbec9015f9
spaghettimedic <dsteiner247@gmail.com>
2021-12-31 14:18:09 -0500

    started routes, working on dept budget route

--- routes/apiRoutes/deptRoutes.js
+++ routes/apiRoutes/deptRoutes.js
@@ -1,7 +1,39 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const db = require('../../db/connection');
 
-// GET all Departments
+// READ all Departments
 router.get('/departments', (req, res) => {
+  const sql = `SELECT * FROM departments`;
   
-})
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// READ department budgets
+router.get('/departments/:id', (req, res) => {
+  const sql = `SELECT SUM(salary)
+              FROM employees
+              WHERE role_id = `;
+  
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('./db/connection');
-// const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,14 +9,8 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World'
-  });
-});
-
 // Use apiRoutes
-// app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
